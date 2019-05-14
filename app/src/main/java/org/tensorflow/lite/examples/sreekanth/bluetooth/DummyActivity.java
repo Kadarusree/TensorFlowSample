07648edd0888d852ced0aecd3ab28dd9545760bd
Srikanth-Dell-PC <ksreekanthyadav@gmail.com>
2019-05-14 21:56:45 +0530

    Debug APK Installed

---
+++ app/src/main/java/org/tensorflow/lite/examples/sreekanth/bluetooth/DummyActivity.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.sreekanth.bluetooth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.tensorflow.lite.examples.sreekanth.R;
+
+
+public class DummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
